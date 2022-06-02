7a177b770993e3a7a11b92c6275d0099a3f41a4d
LuisStuermer <93213675+LuisStuermer@users.noreply.github.com>
2022-06-02 12:30:35 -0300

    Add files via upload

---
+++ includes/_dados.php
@@ -0,0 +1,19 @@
+<?php
+
+$produtos = array(
+    0 => array('nome'=>'cavalo','preco'=>'1200','descricao'=>'descricao vacalo','imagem'=>'cavalo.jpg'),
+    1 => array('nome'=>'','preco'=>'','descrição'=>'','imagem'=>''), 
+    2 => array('nome'=>'','preco'=>'','descrição'=>'','imagem'=>''),
+    3 => array('nome'=>'','preco'=>'','descrição'=>'','imagem'=>''),
+    4 => array('nome'=>'','preco'=>'','descrição'=>'','imagem'=>''),
+    5 => array('nome'=>'','preco'=>'','descrição'=>'','imagem'=>''),
+    6 => array('nome'=>'','preco'=>'','descrição'=>'','imagem'=>''),
+    7 => array('nome'=>'','preco'=>'','descrição'=>'','imagem'=>''),
+    8 => array('nome'=>'','preco'=>'','descrição'=>'','imagem'=>''),
+    9 => array('nome'=>'','preco'=>'','descrição'=>'','imagem'=>''), 
+);
+
+    
+
+
+?>
