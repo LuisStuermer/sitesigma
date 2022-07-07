15f7d04debb1bd085b5e2fdeecd698f616204645
Arthur15y <luispstuermer2004@outlook.com>
2022-07-07 12:22:56 -0300

    Biel 0707

--- index.php
+++ index.php
@@ -4,9 +4,30 @@ include_once './includes/_dados.php';
 include_once './includes/_head.php';
 include_once './includes/_header.php';
 ?>
+<div class="container">
+<h2>Promoção</h2>
 
-<h1>Home</h1>
+    <div class="row mt-3">
+    <?php
+    for ($i=0; $i < 50 ; $i++) { 
+    ?>
 
+    <div class="card m-3" style="width: 12rem;">
+        <img src="./content/<?php echo $produtos[$i]['imagem']?>" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title"><?php echo $produtos[$i]['nome']?></h5>
+            <p class="card-text"><?php echo $produtos[$i]['descricao']?></p>
+            <a href="produto-detalhe.php?id=<?php echo $i;?>" class="btn btn-primary">Comprar</a>
+        </div>
+    </div>
+
+    <?php
+    }
+    ?>
+
+</div>
+</div>
+    
 <?php
 // include do footer
 include_once './includes/_footer.php';
--- produto-detalhe.php
+++ produto-detalhe.php
@@ -3,9 +3,26 @@
 include_once './includes/_dados.php';
 include_once './includes/_head.php';
 include_once './includes/_header.php';
-?>
 
-<h1>Produto Detalhe</h1>
+$id = $_GET['id'];
+?>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col">
+            <h1><?php echo $produtos[$id]['nome'];?></h1>
+            <img src="./content/<?php echo $produtos[$id]['imagem']?>">
+            <h3><?php echo $produtos[$id]['preco']?></h3>
+            <p><?php echo $produtos[$id]['descricao']?></p>
+
+        </div>
+    </div>
+        
+            <div class="row">
+            <div class="col">Voltar</div>
+            <div class="col">Comprar</div>
+        </div>
+        </div>
+</div>
 
 <?php
 // include do footer
